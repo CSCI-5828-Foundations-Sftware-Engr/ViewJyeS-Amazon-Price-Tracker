f2238ca78e585b3c558e617ae07d9953b60e491e
JANUSHI SHASTRI <jash6891@colorado.edu>
2023-04-18 23:40:12 -0600

    Integrated the vue router for using the firebase auth.

--- Frontend/Amazon-Tracker-Frontend/src/layouts/RegisterLayout.vue
+++ Frontend/Amazon-Tracker-Frontend/src/layouts/RegisterLayout.vue
@@ -40,3 +40,73 @@
     </form>
   </div>
 </template>
+
+<script>
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth" ;
+import { useRouter } from "vue-router";
+import axios from "axios";
+import {  useStore } from "vuex";
+
+export default {
+  name: "RegisterLayout",
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const fullName = ref("");
+    const router = useRouter();
+    const auth = getAuth();
+    const errMsg = ref();
+    const apiUrl = "http://127.0.0.1:5000/register";
+    const store = useStore();
+    const registerUser = () => {
+      createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
+          console.log("Successfully registered!");
+          store.dispatch('amazon/updateFullName',fullName.value)
+          store.dispatch('amazon/updateEmailId',email.value)
+          registerUserInDB(email.value, fullName.value);
+          router.push("/search");
+        }).catch((error) => {
+        console.log(error.code);
+        alert(error.message);
+      });
+    };
+
+    const registerUserInDB = async (email, fullName) => {
+      console.log("in the db function");
+      try {
+        const response = await axios.put(apiUrl, {
+          email,
+          fullName
+        });
+        console.log(response.data); // logs the response from the server
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const signInWithGoogle = () => {
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(getAuth(), provider).then((result) => {
+        console.log(result.user);
+        router.push("/search");
+      }).catch((error) => {
+        // handle error
+        console.log(error);
+      });
+    };
+    return {
+      registerUser,
+      signInWithGoogle,
+      email,
+      password,
+      errMsg,
+      fullName
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
