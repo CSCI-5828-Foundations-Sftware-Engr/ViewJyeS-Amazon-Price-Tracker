6d29329dcb86b21225a85cee234c405e1c86ca11
Akhil Gandhi <111824956+AkGandhi99@users.noreply.github.com>
2023-05-04 22:21:56 -0600

    Update test.py

--- Backend/test/test.py
+++ Backend/test/test.py
@@ -9,11 +9,8 @@ def test_register():
     # Check the response status code
     assert response.status_code == 200
 
-    # Check the response data
     assert b'Content updated' in response.data
 
-    # Check if the user_info table in the database is updated
-    # You can add database assertions here to check if the query was executed successfully
 
 
 def test_login():
@@ -21,16 +18,12 @@ def test_login():
     app.testing = True
     client = app.test_client()
 
-    # Send a PUT request to the "/login" endpoint with JSON data
     response = client.put('/login', json={'email': 'test@example.com'})
 
-    # Check the response status code
     assert response.status_code == 200
 
-    # Check the response data
     assert b'full_name' in response.data
 
-    # You can add assertions here to check the returned data from the database query
 
 
 def test_track():
@@ -52,7 +45,6 @@ def test_track():
     assert b'currentPrice' in response.data
     assert b'productLink' in response.data
 
-    # You can add assertions here to check the returned data from the database query
 
 def test_fetchCards():
     app = Flask(__name__)
@@ -65,11 +57,9 @@ def test_fetchCards():
     # Check the response status code
     assert response.status_code == 200
 
-    # Check the response data
     assert b'ASIN' in response.data
     assert b'amazon_link' in response.data
     assert b'product_name' in response.data
     assert b'image_link' in response.data
     assert b'curr_price' in response.data
 
-    # You can add assertions here to check the returned data from the database query
